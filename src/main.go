aa47032a036a4ad6dcd0b1e41e1b3990bc5e7030
pixelatedbus <luthfihakimyusra@gmail.com>
2025-05-06 13:35:05 +0700

    feat: recipe BFS

--- src/main.go
+++ src/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	recipe "github.com/Henshou/Tubes2_BE_CraftingTable.git/recipe"
 	scraper "github.com/Henshou/Tubes2_BE_CraftingTable.git/scraper"
 )
 
@@ -10,5 +12,12 @@ func main() {
 
 	// Get recipes for every element
 	scraper.FindRecipes()
-
+	var err error
+	recipe.RecipeMap, err = recipe.ReadJson("recipes.json")
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return
+	}
+	name := recipe.RecipeMap["Fire"].Name
+	fmt.Println("Name:", name)
 }
