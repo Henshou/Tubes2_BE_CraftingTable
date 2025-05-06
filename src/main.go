e71932d9e78b4f9c948a750baaf68c678f71b9d2
Henshou <gerrardgrs@gmail.com>
2025-05-06 11:31:38 +0700

    Refactor: adjust scraper

--- src/main.go
+++ src/main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	scrapper "github.com/Henshou/Tubes2_BE_CraftingTable.git/scraper"
+	scraper "github.com/Henshou/Tubes2_BE_CraftingTable.git/scraper"
 )
 
 func main() {
 	fmt.Println("Starting Little Alchemy 2 recipe finder...")
 
-	// Choose the element you want to find recipes for
-	targetElement := "Dust"
+	// Get recipes for every element
+	scraper.FindRecipes()
 
-	// Get recipes for the target element
-	recipes := scrapper.FindRecipes(targetElement)
-
-	if len(recipes) == 0 {
-		fmt.Printf("No recipes found for '%s'\n", targetElement)
-		return
-	}
-
-	fmt.Printf("Found %d recipes for '%s':\n", len(recipes), targetElement)
-	for i, recipe := range recipes {
-		fmt.Printf("  %d. %s + %s\n", i+1, recipe[0], recipe[1])
-	}
 }
--- src/scraper/scraper.go
+++ src/scraper/scraper.go
@@ -1,36 +1,70 @@
 package scraper
 
 import (
+	"encoding/json"
+	"log"
+	"os"
 	"strings"
-
 	"github.com/gocolly/colly"
 )
 
-func FindRecipes(element string) [][]string {
-	var recipes [][]string
+type ElementWithRecipes struct {
+	Element string     `json:"element"`
+	Recipes [][]string `json:"recipes"`
+}
+
+func FindRecipes() {
+	var elements []ElementWithRecipes
+
+	baseElements := map[string]bool{
+		"Fire":  true,
+		"Earth": true,
+		"Water": true,
+		"Air":   true,
+	}
 
 	c := colly.NewCollector()
 
 	c.OnHTML("tr", func(e *colly.HTMLElement) {
-		result := e.ChildText("td:nth-of-type(1) a")
-		if strings.EqualFold(result, element) {
-			e.ForEach("td:nth-of-type(2) li", func(_ int, li *colly.HTMLElement) {
-				var components []string
+		elementName := e.ChildText("td:nth-of-type(1) a")
+		if elementName == "" {
+			return
+		}
 
-				li.ForEach("a", func(_ int, a *colly.HTMLElement) {
-					text := strings.TrimSpace(a.Text)
-					if text != "" {
-						components = append(components, text)
-					}
-				})
-
-				if len(components) == 2 {
-					recipes = append(recipes, components)
+		var recipes [][]string
+		e.ForEach("td:nth-of-type(2) li", func(_ int, li *colly.HTMLElement) {
+			var components []string
+			li.ForEach("a", func(_ int, a *colly.HTMLElement) {
+				text := strings.TrimSpace(a.Text)
+				if text != "" {
+					components = append(components, text)
 				}
+			})
+			if len(components) == 2 {
+				recipes = append(recipes, components)
+			}
+		})
+
+		if len(recipes) > 0 && !baseElements[elementName] {
+			elements = append(elements, ElementWithRecipes{
+				Element: elementName,
+				Recipes: recipes,
 			})
 		}
 	})
 
-	c.Visit("https://little-alchemy.fandom.com/wiki/Elements_(Little_Alchemy_2)")
-	return recipes
+	err := c.Visit("https://little-alchemy.fandom.com/wiki/Elements_(Little_Alchemy_2)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData, err := json.MarshalIndent(elements, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("recipes.json", jsonData, 0644)
+	if err != nil {
+		log.Fatal("Failed to write JSON to file:", err)
+	}
 }
